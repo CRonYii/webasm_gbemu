5cfc1d0c610377441ce260056131f19c75dd63e4
Rongyi Chen <kenchenrong@gmail.com>
2020-04-29 11:30:37 -0700

    move all headers to include folder

--- build.ps1
+++ build.ps1
@@ -1,8 +1,9 @@
 $libs_src_path = "./src/libs/**"
+$include_path = "./src/libs/include"
 $web_src_path = "./src/web/**"
 $output_path = "./output"
 $output_name = "gb_lib.js"
 $srcfiles = Get-ChildItem $libs_src_path *.c
 
-emcc -O3 -s WASM=1 -s FORCE_FILESYSTEM=1 -s $srcfiles -o $output_path/$output_name
+emcc -O3 -s WASM=1 -s FORCE_FILESYSTEM=1 -s $srcfiles -I $include_path -o $output_path/$output_name
 Copy-Item $web_src_path $output_path
