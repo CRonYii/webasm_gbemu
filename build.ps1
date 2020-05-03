53e2a326b306c5c7615509c44f58a415ae758c2e
Rongyi Chen <kenchenrong@gmail.com>
2020-05-02 20:02:37 -0700

    better build script

--- build.ps1
+++ build.ps1
@@ -5,5 +5,10 @@ $output_path = "./output"
 $output_name = "gb_lib.js"
 $srcfiles = Get-ChildItem $libs_src_path *.c
 
+If (!(Test-Path $output_path)) {
+    New-Item -ItemType Directory -Force -Path $output_path
+}
+
+emsdk activate latest
 emcc -O3 -s WASM=1 -s FORCE_FILESYSTEM=1 -s ASYNCIFY=1 -s $srcfiles -I $include_path -o $output_path/$output_name
 Copy-Item $web_src_path $output_path
