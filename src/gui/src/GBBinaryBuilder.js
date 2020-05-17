cb6e05752edb9cea7142c7a8a1e1ee5ef9192dc6
Rongyi Chen <kenchenrong@gmail.com>
2020-05-16 19:58:48 -0700

    refactor binary builder

--- src/gui/src/GBBinaryBuilder.js
+++ src/gui/src/GBBinaryBuilder.js
@@ -1,11 +1,10 @@
-import { CloseOutlined, DownOutlined, PlusOutlined, UpOutlined } from '@ant-design/icons';
-import { Button, Col, Input, List, Popconfirm, Row, Tabs } from 'antd';
+import { Button, Col, Row } from 'antd';
 import React from 'react';
 import { BinaryViewer } from './BinaryViewer';
-import { OpcodeInput } from './OpcodeInput';
-import { dataInput } from './RangedNumberInput';
-import { dataRange, delaylock } from './utils';
 import { launchGameboy } from './gb';
+import { OpcodeEditor } from './OpcodeEditor';
+import { OpcodeInput } from './OpcodeInput';
+import { dataRange } from './utils';
 
 const binaryFromOpcode = (op) => {
     const binary = [];
@@ -37,6 +36,18 @@ const populateBinaryPosition = (opcodes) => {
     });
 }
 
+const populateLabels = (opcodes) => {
+    const labels = [];
+    let idx = 0x100;
+    for (const op of opcodes) {
+        if (op.type === 'label') {
+            labels.push({ idx, label: op.label });
+        }
+        idx += op.size;
+    }
+    return labels;
+}
+
 // TODO: save in local storage, one-click run in emualtor
 export class GBBinaryBuilder extends React.Component {
 
@@ -44,63 +55,15 @@ export class GBBinaryBuilder extends React.Component {
         opcodes: [],
         highlightBinary: false,
         binary: getBinary([]),
-        highlightlock: delaylock(10),
-        page: 1
-    }
-
-    opcode_actions = (idx) => [
-        <Popconfirm
-            icon={null} okButtonProps={{ style: { display: 'none' } }} cancelButtonProps={{ style: { display: 'none' } }}
-            title={
-                <Tabs style={{ width: 200 }}>
-                    <Tabs.TabPane tab="Add Opcode" key="1">
-                        <OpcodeInput labels={this.labels()} onSubmit={op => this.addOpcode(op, idx)} />
-                    </Tabs.TabPane>
-                    <Tabs.TabPane tab="Add Label" key="2">
-                        <Input onPressEnter={(evt) => this.addLabel(evt.target.value, idx)} />
-                    </Tabs.TabPane>
-                </Tabs>
-            }
-        ><Button type="primary" shape="round" size="small"><PlusOutlined /></Button></Popconfirm>,
-        <Button disabled={idx === 0} onClick={() => this.swapOpocde(idx, -1)} type="primary" shape="round" size="small"><UpOutlined /></Button>,
-        <Button disabled={idx === this.state.opcodes.length - 1} onClick={() => this.swapOpocde(idx, 1)} type="primary" shape="round" size="small"><DownOutlined /></Button>,
-        <Button onClick={() => this.removeOpcode(idx)} type="primary" danger shape="round" size="small"><CloseOutlined /></Button>
-    ];
-
-    renderListItem = (op, idx) => {
-        const { highlightlock, page } = this.state;
-        idx += (page - 1) * 16;
-        if (op.type === 'opcode') {
-            return <List.Item
-                onMouseEnter={highlightlock(() => this.setBinaryHighlight(idx))}
-                onMouseLeave={highlightlock(() => this.setBinaryHighlight(false))}
-                actions={this.opcode_actions(idx)}
-            >
-                <b>{op.label}</b> {typeof op.data === 'number' ?
-                    <Popconfirm
-                        icon={null}
-                        title={dataInput(op.datatype, { key: op.data, defaultValue: op.data, onPressEnter: (data) => this.updateOpcode(idx, { data }) })}
-                        okButtonProps={{ style: { display: 'none' } }}
-                        cancelButtonProps={{ style: { display: 'none' } }}
-                    >
-                        <Button size="small">{`${op.data}(${dataRange[op.datatype].display(op.data)})`}</Button>
-                    </Popconfirm>
-                    : null}
-            </List.Item>
-        } else if (op.type === 'label') {
-            return <List.Item
-                actions={this.opcode_actions(idx)}
-            >
-                <b style={{ color: '#597cde' }}>{op.label}:</b>
-            </List.Item>
-        }
+        labels: populateLabels([])
     }
 
     updateBinary = (opcodes) => {
         this.setState({
             opcodes,
             binary: getBinary(opcodes),
-            binaryPosition: populateBinaryPosition(opcodes)
+            binaryPosition: populateBinaryPosition(opcodes),
+            labels: populateLabels(opcodes)
         });
     }
 
@@ -110,54 +73,6 @@ export class GBBinaryBuilder extends React.Component {
         if (idx === undefined) idx = opcodes.length;
         opcodes = [...opcodes];
         opcodes.splice(idx, 0, opcode);
-        this.updateBinary(opcodes);
-    }
-
-    addLabel = (label, idx) => {
-        let { opcodes } = this.state;
-        if (idx === undefined) idx = opcodes.length;
-        opcodes = [...opcodes];
-        opcodes.splice(idx, 0, {
-            type: 'label',
-            label,
-            size: 0
-        });
-        this.updateBinary(opcodes);
-    }
-
-    labels = () => {
-        const labels = [];
-        const { opcodes } = this.state;
-        let idx = 0x100;
-        for (const op of opcodes) {
-            if (op.type === 'label') {
-                labels.push({ idx, label: op.label });
-            }
-            idx += op.size;
-        }
-        return labels;
-    }
-
-    removeOpcode = (idx) => {
-        let { opcodes } = this.state;
-        opcodes = [...opcodes];
-        opcodes.splice(idx, 1);
-        this.updateBinary(opcodes);
-    }
-
-    updateOpcode = (idx, op) => {
-        let { opcodes } = this.state;
-        opcodes = [...opcodes];
-        opcodes[idx] = { ...opcodes[idx], ...op }
-        this.updateBinary(opcodes);
-    }
-
-    swapOpocde = (idx, delta) => {
-        let { opcodes } = this.state;
-        opcodes = [...opcodes];
-        const temp = opcodes[idx];
-        opcodes[idx] = opcodes[idx + delta]
-        opcodes[idx + delta] = temp;
         this.updateBinary(opcodes);
     }
 
@@ -177,25 +92,18 @@ export class GBBinaryBuilder extends React.Component {
     }
 
     render() {
-        const { binary, page } = this.state;
+        const { opcodes, labels, binary } = this.state;
         return <div style={{
             width: "80em"
         }}>
-            <OpcodeInput labels={this.labels()} onSubmit={this.addOpcode} dataInputStyle={{ marginLeft: '10px' }} />
+            <OpcodeInput labels={this.state.labels} onSubmit={this.addOpcode} dataInputStyle={{ marginLeft: '10px' }} />
             <Row gutter={24}>
                 <Col span={12}>
-                    <List
-                        pagination={{ pageSize: 16, current: page, onChange: (page) => this.setState({ page }) }}
-                        size="small"
-                        bordered
-                        style={{
-                            fontFamily: 'Consolas',
-                            minHeight: '166px'
-                        }}
-                        dataSource={this.state.opcodes}
-                        renderItem={this.renderListItem}
-                    >
-                    </List>
+                    <OpcodeEditor
+                        opcodes={opcodes}
+                        labels={labels}
+                        updateBinary={this.updateBinary}
+                        setBinaryHighlight={this.setBinaryHighlight} />
                 </Col>
                 <Col span={12}>
                     <BinaryViewer highlightBinary={this.state.highlightBinary} binary={binary.slice(0x100)} />
